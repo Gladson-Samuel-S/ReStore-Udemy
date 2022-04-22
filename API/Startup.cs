bd2315f061446edb064d6d02587410fe96421291
Gladson-Samuel-S <samuelgladson4@gmail.com>
2022-04-22 22:37:51 +0530

    Adding CORS Support

--- API/Startup.cs
+++ API/Startup.cs
@@ -47,6 +47,11 @@ namespace API
 
             app.UseRouting();
 
+            app.UseCors(opt =>
+            {
+                opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+            });
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
